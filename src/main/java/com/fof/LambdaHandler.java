557b0bf809b71aa5cbf2d29eb3fad7fb29bc2995
glen <glen_dovey@hotmail.com>
2024-03-17 19:20:06 +0000

    fix: cloudwatch logs

--- src/main/java/com/fof/LambdaHandler.java
+++ src/main/java/com/fof/LambdaHandler.java
@@ -3,7 +3,6 @@ package com.fof;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 
 public class LambdaHandler implements RequestHandler<Object, Object> {
 
@@ -11,14 +10,14 @@ public class LambdaHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(Object input, Context context) {
         LambdaLogger logger = context.getLogger();
         // Log messages at different levels
-        logger.log("This is an informational message", LogLevel.INFO);
-        logger.log("This is a warning message", LogLevel.WARN);
-        logger.log("This is a severe message", LogLevel.ERROR);
+        logger.log("This is an informational message");
+        logger.log("This is a warning message");
+        logger.log("This is a severe message");
 
         // Example of logging variable values
         String name = "John";
         int age = 30;
-        logger.log("Name: " + name + ", Age: " + age, LogLevel.INFO);
+        logger.log("Name: " + name + ", Age: " + age);
 
         // Your Discord bot initialization and logic here
         App.buildBot(); // Start the bot
